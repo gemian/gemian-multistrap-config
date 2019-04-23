59ee5f607ffbae0b8f72b75a905761da917ce217
Adam Boardman <adamboardman@gmail.com>
2019-04-23 14:03:05 +0100

    Adding gps

--- debian-gemini-config.sh
+++ debian-gemini-config.sh
@@ -31,6 +31,8 @@ ldconfig
 
 groupadd -g 1001 radio
 useradd -u 1001 -g 1001 -s /usr/sbin/nologin radio
+groupadd -g 1021 gps
+useradd -u 1021 -g 1021 -s /usr/sbin/nologin gps
 
 groupadd -g 1000 aid_system
 groupadd -g 1003 aid_graphics
